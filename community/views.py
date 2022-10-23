170005deb1286e5b484c4775523d5f2580c0a73d
poro625 <poro625@naver.com>
2022-10-24 04:38:41 +0900

    게시글 완료

--- community/views.py
+++ community/views.py
@@ -28,3 +28,42 @@ def article_detail(request, article_id):
 
     }
     return render(request, 'article_detail.html', context)
+
+def update_article(request, article_id):
+    article = Article.objects.get(id = article_id)
+
+    if request.user != article.user:
+        return HttpResponse('권한이 없습니다')
+    if request.method == 'GET':         
+        context = {
+
+        'article':article
+
+        }
+
+        return render(request, 'update_article.html', context )
+    elif request.method == 'POST':
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        context = {
+
+        'article':article
+
+        }
+
+        return redirect('community:article_detail', article_id) 
+
+
+def delete_article(request, article_id):
+    
+    if request.user != article.user:
+        return HttpResponse('권한이 없습니다')
+
+    if request.method == 'POST':  
+        article = Article.objects.get(id = article_id)
+        if request.user != article.user:
+            return HttpResponse('권한이 없습니다')        
+        article.delete()
+        return redirect('community:index')
+    
